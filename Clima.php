4f4f26771eda03d8218c6123210d6c76596679e1
Wagner Correia <43221732+wagnercorreiasenai@users.noreply.github.com>
2020-04-08 21:23:07 -0300

    Create Clima.php

---
+++ Clima.php
@@ -0,0 +1,28 @@
+<?php
+
+class Clima {
+
+    public $codCidade;
+    public $cidade;
+    public $temperatura;
+    public $velocidadeVento;
+    public $nascerDoSol;
+    public $porDoSol;
+    public $humidade;
+    public $pressao;
+    public $descricao;
+    public $icone;
+
+    //Converte de kelvin para celsius
+    public function getTemperaturaCelsius() : float {
+        
+    }
+    
+    //Convete de kelvin para fahrenheit
+    public function getTemperaturaFahrenheit() : float {
+        
+    }
+
+}
+
+?>
